40c07a096dd15ccfbad02998d8fbf54c79793164
David Yang <davidkunheeyang@gmail.com>
2023-02-20 15:33:27 -0800

    Initial Commit

---
+++ Currency Display/Currency_DisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_DisplayApp.swift
+//  Currency Display
+//
+//  Created by David Yang on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Currency_DisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
