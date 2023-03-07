1eaf15f47ba9fe908d42c3d1597ef2f691e5941e
David Yang <davidkunheeyang@gmail.com>
2023-03-06 18:27:38 -0800

    initial

--- Currency Display/API/ExchangeAPI.swift
+++ Currency Display/API/ExchangeAPI.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 let EXCHANGERATE_API_ROOT = "https://api.exchangerate.host/"
-let SEARCH_ENDPOINT = "\(EXCHANGERATE_API_ROOT)/latest"
+let LATEST_ENDPOINT = "\(EXCHANGERATE_API_ROOT)/latest"
 
 enum ExchangeAPIError: Error {
     case unsuccessfulDecode
     case badUrl
 }
 
-func getRates(base: String, amount: Double) async throws -> ExchangeRate {
-    guard let url = URL(string: "\(SEARCH_ENDPOINT)?symbols=USD,JPY&amount=\(amount)&base=\(base)") else {
+func getRates(base: String, symbols: String) async throws -> ExchangeRate {
+    guard let url = URL(string: "\(LATEST_ENDPOINT)?symbols=\(symbols)&base=\(base)") else {
         throw ExchangeAPIError.badUrl
     }
 
@@ -26,10 +26,17 @@ func getRates(base: String, amount: Double) async throws -> ExchangeRate {
     } else {
         throw ExchangeAPIError.unsuccessfulDecode
     }
-//    guard let url = URL("\(SEARCH_ENDPOINT)?symbols=USD,JPY") else{
-//        throw ExchangeAPIError.badUrl
-//    }
-//
-//    let(data, _) = try await URLSession.shared.data(from:url)
-//    if
+}
+
+func exchangeAmount(base: String, symbols: String, amount: Double) async throws -> ExchangeRate {
+    guard let url = URL(string: "\(LATEST_ENDPOINT)?base=\(base)&symbols=\(symbols)&amount=\(amount)") else {
+        throw ExchangeAPIError.badUrl
+    }
+
+    let (data, _) = try await URLSession.shared.data(from: url)
+    if let decodedPage = try? JSONDecoder().decode(ExchangeRate.self, from: data) {
+        return decodedPage
+    } else {
+        throw ExchangeAPIError.unsuccessfulDecode
+    }
 }
--- Currency Display/Views/ExchangeListView.swift
+++ Currency Display/Views/ExchangeListView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ExchangeListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            //fetched data
+        }
     }
 }
 
